bf2b144c5cde026cf42064daf82f1c5f5db21d7e
sky-wang@qq.com <sky-wang@qq.com>
2018-06-26 11:02:14 +0800

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# webpack-dist-replace-plugin
+replace your webpack dist content.
+
+## Install
+```bash
+npm install webpack-dist-replace-plugin
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+// webpack-dist-replace-plugin
+// @author sky-wang@qq.com
+
+var RawSource = require('./node_modules/webpack-sources/lib/RawSource');
+
+var options;
+
+function ReplacePlugin(config) {
+    if (!config || !config.replace) {
+        throw new Error('webpack-dist-replace-plugin needs a replace function.');
+    }    
+    options = config;
+}
+
+ReplacePlugin.prototype.apply = function(compiler) {
+    compiler.plugin('emit', function(compilation, callback) {
+        var assets = compilation.assets;
+
+        for (var path in assets) {
+            var content = assets[path].source();
+            if (typeof content === 'string') {
+                assets[path] = new RawSource(
+                    options.replace(content)                    
+                );
+            }
+        }
+
+        callback();
+    });
+};
+
+module.exports = ReplacePlugin;
